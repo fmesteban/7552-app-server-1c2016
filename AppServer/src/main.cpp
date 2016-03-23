bbfde68a982f300e11283581ee9a603a6511377a
eze210 <eze210@gmail.com>
2016-03-23 20:43:21 -0300

    Adding rocksdb support.

--- AppServer/src/main.cpp
+++ AppServer/src/main.cpp
@@ -1,7 +1,61 @@
+/*
 #include "WebServer.h"
+#include "rocksdb/c.h"
 
 int main(void) {
 	WebServer server;
 	server.start();
 	return 0;
+}
+*/
+#include <cstdio>
+#include <string>
+
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+
+//using namespace rocksdb;
+
+std::string kDBPath = "/tmp/rocksdb_simple_example";
+
+int main() {
+  rocksdb::DB* db;
+  rocksdb::Options options;
+  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+  options.IncreaseParallelism();
+  options.OptimizeLevelStyleCompaction();
+  // create the DB if it's not already present
+  options.create_if_missing = true;
+
+  // open DB
+  rocksdb::Status s = rocksdb::DB::Open(options, kDBPath, &db);
+  assert(s.ok());
+
+  // Put key-value
+  s = db->Put(rocksdb::WriteOptions(), "key1", "value");
+  assert(s.ok());
+  std::string value;
+  // get value
+  s = db->Get(rocksdb::ReadOptions(), "key1", &value);
+  assert(s.ok());
+  assert(value == "value");
+
+  // atomically apply a set of updates
+  {
+	rocksdb::WriteBatch batch;
+    batch.Delete("key1");
+    batch.Put("key2", value);
+    s = db->Write(rocksdb::WriteOptions(), &batch);
+  }
+
+  s = db->Get(rocksdb::ReadOptions(), "key1", &value);
+  assert(s.IsNotFound());
+
+  db->Get(rocksdb::ReadOptions(), "key2", &value);
+  assert(value == "value");
+
+  delete db;
+
+  return 0;
 }
--- AppServer/src/web_server/WebServer.cpp
+++ AppServer/src/web_server/WebServer.cpp
@@ -1,5 +1,6 @@
 #include "WebServer.h"
 #include <iostream>
+#include <string>
 
 
 int WebServer::is_websocket(const struct mg_connection *nc) {
@@ -16,24 +17,39 @@ void WebServer::broadcast(struct mg_connection *nc, const char *msg, size_t len)
 	}
 }
 
-static void eventHandler(struct mg_connection *nc, int ev, void *p) {
+static void eventHandler( struct mg_connection *nc, int ev, void *p ) {
 	WebServer* self = (WebServer *) nc->user_data;
-	struct http_message *hm = (struct http_message *) p;
-	struct websocket_message *wm = (struct websocket_message *) p;
+	struct http_message *httpMessage = (struct http_message *) p;
+	struct websocket_message *websocketMessage = (struct websocket_message *) p;
 
 	switch (ev) {
 	case MG_EV_HTTP_REQUEST:
+	{
+		std::string s1(httpMessage->uri.p);
+		std::string s = s1.substr(0, httpMessage->uri.len);
+		std::cout << "http" << s << std::endl;
+		if(mg_vcmp(&httpMessage->uri, "/api/post/number") == 0){
+			char buf[100];
+			/* Get form variables */
+			mg_get_http_var(&httpMessage->body, "number", buf, sizeof(buf));
+			std::cout << "receiving: " << buf << std::endl;
+			mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
+			mg_printf_http_chunk(nc, "{ \"response\": %lf }", strtod(buf, NULL)*strtod(buf, NULL));
+			mg_send_http_chunk(nc, "", 0);
+			break;
+		}
 		/* Usual HTTP request - serve static files */
-		mg_serve_http(nc, hm, self->s_http_server_opts);
+		mg_serve_http(nc, httpMessage, self->s_http_server_opts);
 		nc->flags |= MG_F_SEND_AND_CLOSE;
 		break;
+	}
 	case MG_EV_WEBSOCKET_HANDSHAKE_DONE:
 		/* New websocket connection. Tell everybody. */
 		self->broadcast(nc, "joined", 6);
 		break;
 	case MG_EV_WEBSOCKET_FRAME:
 		/* New websocket message. Tell everybody. */
-		self->broadcast(nc, (char *) wm->data, wm->size);
+		self->broadcast(nc, (char *) websocketMessage->data, websocketMessage->size);
 		break;
 	case MG_EV_CLOSE:
 		/* Disconnect. Tell everybody. */
