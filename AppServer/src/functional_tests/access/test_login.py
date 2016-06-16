e822066bcbff176ae3582598b830f1cbf181972e
federico <fede.est@gmail.com>
2016-06-16 01:06:40 -0300

    Added tests.

---
+++ AppServer/src/functional_tests/access/test_login.py
@@ -0,0 +1,45 @@
+#encoding=utf-8
+import requests
+import unittest
+import json
+from subprocess import call
+import inspect, os
+
+n = 45
+mail = "example_login" + str(n) + "@mail.com"
+
+class TestLogin(unittest.TestCase):
+
+	def test_login_existing_user(self):
+		data = json.dumps({"name": "TestLogin", "alias": "usuario_login", "password": "test", "email": mail, 
+			"birthday": "10/10/10", "sex": "Male", "location": { "latitude": 45, "longitude": 46 }, "photo_profile": "base64photo" })
+		requests.post("http://localhost:8000/register", data = data)
+
+		data = {"email": mail, "password": "test"}
+		r = requests.post("http://localhost:8000/login", data = json.dumps(data))
+		self.assertEqual(r.status_code, 201)
+		#Assert the alias is the same
+	    alias = r.json()[u'user'][u'alias']
+	    self.assertEqual(alias, "usuario_login")
+
+	def test_login_nonexisting_user(self):
+		data = {"email": "example@inexistent.com", "password": "test"}
+		r = requests.post("http://localhost:8000/login", data = json.dumps(data))
+		self.assertEqual(r.status_code, 500)
+
+	def test_login_invalid_arguments(self):
+		data = {"email": mail}
+		r = requests.post("http://localhost:8000/login", data = json.dumps(data))
+		self.assertEqual(r.status_code, 400)
+
+	def test_login_invalid_password(self):
+		data = json.dumps({"name": "TestWronsPasswd", "alias": "usuario_wpasswd", "password": "test", "email": mail, 
+			"birthday": "10/10/10", "sex": "Male", "location": { "latitude": 45, "longitude": 46 }, "photo_profile": "base64photo" })
+		requests.post("http://localhost:8000/register", data = data)
+
+		data = {"email": "example@wrongpasswd.com", "password": "wrong"}
+		r = requests.post("http://localhost:8000/login", data = json.dumps(data))
+		self.assertEqual(r.status_code, 400)
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ AppServer/src/functional_tests/matches/test_possible_matches.py
@@ -0,0 +1,14 @@
+#encoding=utf-8
+import requests
+import unittest
+import json
+from subprocess import call
+import inspect, os
+
+class TestPossibleMatches(unittest.TestCase):
+
+	
+
+
+if __name__ == '__main__':
+	unittest.main()
